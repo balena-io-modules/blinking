54aa604c3877eddb47791e37ee0bc9565e046227
Pagan Gazzard <page@balena.io>
2021-04-16 12:25:35 +0100

    Convert to typescript
    Change-type: patch

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blinking
+
+This is a module that allows specifying a file path and a pattern and
+will then write 0/1 according to the pattern. This is useful in
+particular for blinking the led on raspberry pis as the led can be
+controlled via a file, although it will act the same for any file path
+that is provided
---
+++ index.ts
@@ -0,0 +1,67 @@
+import * as Promise from 'bluebird';
+import { writeFile } from 'fs';
+
+const writeFileAsync = Promise.promisify(writeFile);
+Promise.config({
+	// Enable cancellation
+	cancellation: true,
+});
+
+type Pattern = {
+	blinks: number;
+	onDuration: number;
+	offDuration: number;
+	pause: number;
+};
+
+const numToArray = (n: number) => {
+	const r = new Array(n);
+	for (let i = 0; i < n; i++) {
+		r[i] = undefined;
+	}
+	return r;
+};
+
+// Helps in blinking the LED from the given end point.
+export = (ledFile: string) => {
+	const ledOn = () => writeFileAsync(ledFile, 1);
+	const ledOff = () => writeFileAsync(ledFile, 0);
+
+	const blink = (ms: number = 200) => {
+		ms ??= 200;
+		return ledOn().delay(ms).then(ledOff);
+	};
+
+	let blinking: null | Promise<void> = null;
+	const start = (pattern: Pattern): Promise<void> =>
+		Promise.resolve(numToArray(pattern.blinks))
+			.each(() => blink(pattern.onDuration).delay(pattern.offDuration))
+			.delay(pattern.pause)
+			.then(() => start(pattern));
+
+	blink.pattern = {
+		start(pattern: Partial<Pattern> = {}) {
+			pattern ??= {};
+			if (blinking != null) {
+				return false;
+			}
+			const fullPattern = {
+				blinks: pattern.blinks ?? 1,
+				onDuration: pattern.onDuration ?? 200,
+				offDuration: pattern.offDuration ?? 200,
+				pause: pattern.pause ?? 0,
+			};
+			blinking = start(fullPattern);
+		},
+		stop() {
+			if (blinking == null) {
+				return false;
+			}
+			blinking.cancel();
+			ledOff();
+			blinking = null;
+		},
+	};
+
+	return blink;
+};
